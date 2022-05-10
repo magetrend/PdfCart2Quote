aa6929cafb2c8e7e6d83e99b0a259577221a53fd
Edvinas Stulpinas <edvinas.stulpinas@gmail.com>
2022-05-10 12:00:22 +0300

    - Fixed: Impossible to process constructor argument Parameter #0 [ Magetrend\PdfCart2Quote\Model\Quote\Pdf $quotePdf ] of Magetrend\PdfCart2Quote\Plugin\Model\Sales\Order\Pdfclass

---
+++ Model/Quote/Pdf.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MB "Vienas bitas" (Magetrend.com)
+ *
+ * @category MageTrend
+ * @package  Magetend/PdfCart2Quote
+ * @author   Edvstu <edwin@magetrend.com>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     https://www.magetrend.com/magento-2-pdf-invoice-pro
+ */
+
+namespace Magetrend\PdfCart2Quote\Model\Quote;
+
+class Pdf extends \Cart2Quote\Quotation\Model\Quote\Pdf\Quote
+{
+   public function getTotalsList()
+   {
+       return $this->_getTotalsList();
+   }
+}
